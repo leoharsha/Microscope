645d18ef6b065e19f675c8f6b620e0f413cc528d
Leo Harsha <leo.harsha10@gmail.com>
2016-08-19 08:13:04 +0530

    Initial

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,21 @@
+var postsData = [
+  {
+    title: 'Leo Messi Legacy',
+    author:'Lionel Messi',
+    url:'http://leoharsha.com/'
+  },
+  {
+    title: 'Neymar Legacy',
+    author:'Neymar Jr',
+    url:'http://leoharsha.com/'
+  },
+  {
+    title: 'Suarez Legacy',
+    author:'Luis Suarez',
+    url:'http://leoharsha.com/'
+  }
+
+];
+Template.postsList.helpers({
+posts:postsData
+});
